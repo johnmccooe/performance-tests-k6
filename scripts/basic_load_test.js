38e78728a348772bed1039423255db4128e1712f
John McCooe <johnmccooe@gmail.com>
2025-12-17 15:57:24 +0000

    Phase 1: Added custom Counter metric and threshold

--- scripts/basic_load_test.js
+++ scripts/basic_load_test.js
@@ -1,5 +1,9 @@
 import http from 'k6/http';
 import { check, sleep, group } from 'k6';
+import { Counter } from 'k6/metrics'; // 1. Import the Counter
+
+// 2. Define the counter
+const loginCounter = new Counter('successful_logins');
 
 export const options = {
   //vus: 5,
@@ -12,6 +16,8 @@ export const options = {
   thresholds: {
     http_req_failed: ['rate<0.01'], // Fail if more than 1% of requests fail
     http_req_duration: ['p(95)<150'], // Fail if 95% of requests take longer than 200ms
+    // 3. You can even set a threshold on your custom counter!
+    'successful_logins': ['count>50'],
   },
 };
 
@@ -19,21 +25,19 @@ export default function () {
 
   group('BP01_Homepage_Load', function () {
     const res = http.get('https://test.k6.io');
-    
-    check(res, {
-      'status is 200': (r) => r.status === 200,
-      'verify homepage text': (r) => r.body.includes('Collection of simple web-pages'),
-    });
+    check(res, {'status is 200': (r) => r.status === 200,});
   });
 
   sleep(1);
 
   group('BP02_Login_Page_Access', function () {
     const res = http.get('https://test.k6.io/my_messages.php');
+    const isOk = check(res, { 'login status is 200': (r) => r.status === 200 });
     
-    check(res, {
-      'login page status is 200': (r) => r.status === 200,
-    });
+    // 3. Only count it if the check passed
+    if (isOk) {
+      loginCounter.add(1);
+    }
   });
 
   sleep(1);
